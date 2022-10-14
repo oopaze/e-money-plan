0eac74a0b2c61e5189ac79f98b1888902e7412db
José Pedro <pedroosd28@gmail.com>
2022-10-14 01:30:58 -0300

    fix: renaming var config from heroku database

--- app/config/production.py
+++ app/config/production.py
@@ -6,4 +6,4 @@ from ..config import BaseConfig
 class ProductionConfig(BaseConfig):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")

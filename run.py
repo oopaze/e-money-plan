091d3c42d73a74604806f6d2b7ddc2a427b8583c
José Pedro <pedroosd28@gmail.com>
2022-10-14 00:36:31 -0300

    feat: adding healthcheck route

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 from dotenv import load_dotenv
+from flask import jsonify
 
 from app import create_app
 
 app = create_app()
+
+
+@app.route("/healthcheck")
+def healthcheck():
+    return jsonify({"status": "on"}), 200
+
 
 if __name__ == "__main__":
     load_dotenv(".env")
